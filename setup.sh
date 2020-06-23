aa84a984afb8488ea8b73498ec5277a3192d3c94
luisf <luisfcomonge@gmail.com>
2020-06-23 09:17:31 +0200

    setup fixed

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ sudo -u postgres psql -q -c "create database grafiki;"
 sudo -u postgres psql -d grafiki -a -f initial.sql
 
 # Install dependencies
-apt install python3, python3-pip, django, libpq-dev, python3-psycopg2, python-djangorestframework, python-django-crispy-forms, python-evtx -y
-pip3 install elasticsearch_dsl
+sudo apt install python3 python3-pip libpq-dev -y
+pip3 install elasticsearch_dsl django psycopg2 djangorestframework django-crispy-forms evtx
