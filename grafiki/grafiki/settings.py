cfbcb76030430ea5f25473fb5fa092993104ac72
svch0stz <8684257+svch0stz@users.noreply.github.com>
2023-05-28 14:05:05 +1000

    Update settings.py

--- grafiki/grafiki/settings.py
+++ grafiki/grafiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j-)4yu(bse8jw4f5v+6-%$f$v1t7#f6a7ug%(0%!=0nvux+sv&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'grafiki.local']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'grafiki.local']
 
 
 # Application definition
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
